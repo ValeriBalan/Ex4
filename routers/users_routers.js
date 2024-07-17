8868d4cb2c84b27409fdaee00589ccc4a80d951f
ValeriBalan <165626222+ValeriBalan@users.noreply.github.com>
2024-07-17 17:15:49 +0300

    connection node

--- routers/users_routers.js
+++ routers/users_routers.js
@@ -3,8 +3,8 @@ const { users_controller } = require('../controllers/users_controller.js');
 
 const usersRouter = new Router();
 usersRouter.get('/', users_controller.getUser);
-usersRouter.post('/', users_controller.addUser);
-usersRouter.get('/', users_controller.generateUniqueAccessCode);
-usersRouter.get('/', users_controller.generateAccessCode);
+usersRouter.post('/add', users_controller.addUser);
+usersRouter.get('/generateUniqueAccessCode', users_controller.generateUniqueAccessCode);
+usersRouter.get('/generateAccessCode', users_controller.generateAccessCode);
 
 module.exports =  {usersRouter} ;
