67f0713d8a070631071c837ee827419c5cd5e5c5
ValeriBalan <165626222+ValeriBalan@users.noreply.github.com>
2024-07-17 15:30:19 +0300

    user controller+ router

--- controllers/users_controller.js
+++ controllers/users_controller.js
@@ -1,4 +1,4 @@
-exports.usersController = {
+exports.users_controller = {
     async getUser(req, res) {
         const { dbConnection } = require('../db_connection');
         try {
@@ -11,4 +11,48 @@ exports.usersController = {
             console.error('Error fetching users:', error);
             res.status(500).json({ success: false, message: 'Internal Server Error' });
         }
-    }};
+    },
+    async addUser(req, res) {
+        const { dbConnection } = require('../db_connection');
+        const connection = await dbConnection.createConnection();
+        const { users_controller } = require('./users_controller');
+        const { generateUniqueAccessCode } = users_controller;
+        const { body } = req;
+        try {
+            let [accessCode] = await generateUniqueAccessCode(connection);
+    
+            const result = await connection.execute(
+                `INSERT INTO tbl_26_users (user_name, user_password, access_code) 
+                 VALUES (?, ?, ?)`,
+                [body.user_name, body.user_password, accessCode]
+            );
+    
+            connection.end();
+            res.send(true);
+        } catch (error) {
+            console.error('Error inserting user:', error);
+            res.status(500).send(false);
+        }
+    },
+    async generateUniqueAccessCode(connection) {
+        const { users_controller } = require('./users_controller');
+        const { generateAccessCode } = users_controller;
+        while (true) {
+            const [accessCode] = await generateAccessCode();
+    
+            const [rows] = await connection.execute(
+                `SELECT COUNT(*) AS count FROM tbl_26_users WHERE access_code = ?`,
+                [accessCode]
+            );
+    
+            if (rows[0].count === 0) {
+                return accessCode; 
+            }
+        }
+    },
+    async generateAccessCode() {
+        const randomString = Math.random().toString(36).substring(2, 10);
+        return randomString;
+    }
+};
+
--- routers/users_routers.js
+++ routers/users_routers.js
@@ -3,5 +3,8 @@ const { users_controller } = require('../controllers/users_controller.js');
 
 const usersRouter = new Router();
 usersRouter.get('/', users_controller.getUser);
+usersRouter.post('/', users_controller.addUser);
+usersRouter.get('/', users_controller.generateUniqueAccessCode);
+usersRouter.get('/', users_controller.generateAccessCode);
 
-module.exports = { usersRouter };
+module.exports =  {usersRouter} ;
