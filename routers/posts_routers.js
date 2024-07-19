85d4c20f4873e621b2a6f13c39a5ea8ba0403d1e
ValeriBalan <165626222+ValeriBalan@users.noreply.github.com>
2024-07-19 13:39:56 +0300

    spelling

--- routers/posts_routers.js
+++ routers/posts_routers.js
@@ -1,7 +1,7 @@
 const { Router } = require('express');
 const { posts_controller } = require('../controllers/posts_controller.js');
 
-const PostsRouter = new Router();
+const postsRouter = new Router();
 postsRouter.get('/getAllPosts', posts_controller.getPosts);
 //usersRouter.post('/add', users_controller.addUser);
 //usersRouter.get('/generateUniqueAccessCode', users_controller.generateUniqueAccessCode);
