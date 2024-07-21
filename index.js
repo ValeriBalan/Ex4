573e4a3297d9ff93786f5df2da51ed02b17273f4
ValeriBalan <165626222+ValeriBalan@users.noreply.github.com>
2024-07-21 23:02:28 +0300

    spelling mistakes

--- index.js
+++ index.js
@@ -5,7 +5,6 @@ const app = express();
 const port = process.env.PORT || 8081;
 const { usersRouter } = require('./routers/users_routers.js');
 const { postsRouter } = require('./routers/posts_routers.js');
-
 const vacation_preferences = require('./data/vacation_preferences.json');
 
 app.use((req, res, next) => {
@@ -19,33 +18,26 @@ app.use((req, res, next) => {
 });
 
 app.use(express.json());
+app.use(bodyParser.json());
+app.use(express.urlencoded({ extended: true }));
+
 app.get('/', (req, res) => {
     res.send('Welcome to the User Management API');
 });
-app.get("/api/vacation_preferences", (req,res) =>{
+
+app.get("/api/vacation_preferences", (req, res) => {
     res.json(vacation_preferences);
     console.log(vacation_preferences);
 });
-app.use(bodyParser.json());
-app.use(express.urlencoded({ extended: true }));
+
 app.use('/api/users', usersRouter);
 app.use('/api/posts', postsRouter);
 
 app.use((req, res) => {
     console.error('Path not found:', req.path);
-    res.status(400).send('something is broken!');
+    res.status(404).send('Path not found!');
 });
 
 app.listen(port, () => {
-    console.log(`Listening on port ${port}`);
-});
-
-app.get('/calculate-vacation-results', async (req, res) => {
-    const result = await posts_controller.calculateVacationResults();
-    res.json(result);
-});
-
-const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => {
-    console.log(`Server is running on port ${PORT}`);
+    console.log(`Server is running on port ${port}`);
 });
--- routers/posts_routers.js
+++ routers/posts_routers.js
@@ -1,13 +1,12 @@
-const express = require('express'); 
-const router = express.Router(); 
-const { posts_controller } = require('../controllers/posts_controller.js');
+const express = require('express');
+const postsRouter = express.Router();
+const posts_controller = require('../controllers/posts_controller');
 
-const postsRouter = new Router();
 postsRouter.get('/getAllPosts', posts_controller.getPosts);
 postsRouter.post('/updatePreference', posts_controller.updatePreference);
 postsRouter.post('/addPreference', posts_controller.addPreference);
-router.get('/calculate-vacation-results', async (req, res) => {
+postsRouter.get('/calculate-vacation-results', async (req, res) => {
     const result = await posts_controller.calculateVacationResults();
     res.json(result);
 });
-module.exports = { router };
+module.exports = { postsRouter };
