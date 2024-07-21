743cb8a53401f43c1e7c101b4e24db7fb28ff9ac
ValeriBalan <165626222+ValeriBalan@users.noreply.github.com>
2024-07-21 23:37:07 +0300

    update in index

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
 require('dotenv').config();
 const express = require('express');
-const bodyParser = require('body-parser');
 const app = express();
 const port = process.env.PORT || 8081;
 const { usersRouter } = require('./routers/users_routers.js');
 const { postsRouter } = require('./routers/posts_routers.js');
+
 const vacation_preferences = require('./data/vacation_preferences.json');
 
 app.use((req, res, next) => {
@@ -18,17 +18,13 @@ app.use((req, res, next) => {
 });
 
 app.use(express.json());
-app.use(bodyParser.json());
-app.use(express.urlencoded({ extended: true }));
-
 app.get('/', (req, res) => {
     res.send('Welcome to the User Management API');
 });
 app.get("/api/vacation_preferences", (req,res) =>{
     res.json(vacation_preferences);
-    console.log(vacation_preferences);
+    console.log(vacation_preferences)
 });
-app.use(bodyParser.json());
 app.use(express.urlencoded({ extended: true }));
 app.use('/api/users', usersRouter);
 app.use('/api/posts', postsRouter);
@@ -40,14 +36,4 @@ app.use((req, res) => {
 
 app.listen(port, () => {
     console.log(`Listening on port ${port}`);
-});
-
-app.get('/calculate-vacation-results', async (req, res) => {
-    const result = await posts_controller.calculateVacationResults();
-    res.json(result);
-});
-
-const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => {
-    console.log(`Server is running on port ${PORT}`);
 });
